21eb52a4f78a166b76ef290abf8fa324e7f37012
Mylo474 <antwieric@gmail.com>
2023-03-07 11:10:10 +0000

    add css style

--- index.js
+++ index.js
@@ -1,80 +1,66 @@
-let homeMenu = document.getElementById('humMenu');
-let navCo = document.getElementById('nav_list_container');
-let closeBtn = document.getElementById('close-btn');
-let body = document.getElementById('body');
-let homeBtn = document.getElementById('home-btn');
-let projectsBtn = document.getElementById('projects-btn');
-let aboutBtn = document.getElementById('about-btn');
-let contactBtn = document.getElementById('contact-btn');
-let headerDesign = document.getElementById('shape2')
-let shape1 = document.getElementById('shape1');
-
+const homeMenu = document.getElementById('humMenu');
+const navCo = document.getElementById('nav_list_container');
+const closeBtn = document.getElementById('close-btn');
+const body = document.getElementById('body');
+const homeBtn = document.getElementById('home-btn');
+const projectsBtn = document.getElementById('projects-btn');
+const aboutBtn = document.getElementById('about-btn');
+const contactBtn = document.getElementById('contact-btn');
+const headerDesign = document.getElementById('shape2');
+const shape1 = document.getElementById('shape1');
 
 homeMenu.addEventListener('click', () => {
-    navCo.style.display = 'flex';
-    navCo.style.height = '100vh';
-    closeBtn.style.display = 'block';
-    homeMenu.style.display = 'none';
-    //logo.style.display = 'none';
-    body.style.position = 'sticky';
-    body.style.overflow = 'hidden';
-    //nav.style.width = '100%';
-    headerDesign.style.display = 'none';
-    shape1.style.display = 'none';
-  } )
-
+  navCo.style.display = 'flex';
+  navCo.style.height = '100vh';
+  closeBtn.style.display = 'block';
+  homeMenu.style.display = 'none';
+  body.style.position = 'sticky';
+  body.style.overflow = 'hidden';
+  headerDesign.style.display = 'none';
+  shape1.style.display = 'none';
+});
 
 closeBtn.addEventListener('click', () => {
-    navCo.style.display = 'none';
-    homeMenu.style.display = 'block';
-    //logo.style.display = 'block';
-    body.style.position = 'static';
-    body.style.overflow = 'auto';
-    headerDesign.style.display = 'block';
-    shape1.style.display = 'block';
-    //nav.style.width = 'auto';
-  } )
+  navCo.style.display = 'none';
+  homeMenu.style.display = 'block';
+  body.style.position = 'static';
+  body.style.overflow = 'auto';
+  headerDesign.style.display = 'block';
+  shape1.style.display = 'block';
+});
 
-  homeBtn.addEventListener('click', () => {
-    navCo.style.display = 'none';
-    homeMenu.style.display = 'block';
-    //logo.style.display = 'block';
-    body.style.position = 'static';
-    body.style.overflow = 'auto';
-    //nav.style.width = 'auto';
-    headerDesign.style.display = 'block';
-    shape1.style.display = 'block';
-  } )
+homeBtn.addEventListener('click', () => {
+  navCo.style.display = 'none';
+  homeMenu.style.display = 'block';
+  body.style.position = 'static';
+  body.style.overflow = 'auto';
+  headerDesign.style.display = 'block';
+  shape1.style.display = 'block';
+});
 
-  projectsBtn.addEventListener('click', () => {
-    navCo.style.display = 'none';
-    homeMenu.style.display = 'block';
-    //logo.style.display = 'block';
-    body.style.position = 'static';
-    body.style.overflow = 'auto';
-    //nav.style.width = 'auto';
-    headerDesign.style.display = 'block';
-    shape1.style.display = 'block';
-  } )
+projectsBtn.addEventListener('click', () => {
+  navCo.style.display = 'none';
+  homeMenu.style.display = 'block';
+  body.style.position = 'static';
+  body.style.overflow = 'auto';
+  headerDesign.style.display = 'block';
+  shape1.style.display = 'block';
+});
 
-  aboutBtn.addEventListener('click', () => {
-    navCo.style.display = 'none';
-    homeMenu.style.display = 'block';
-    //logo.style.display = 'block';
-    body.style.position = 'static';
-    body.style.overflow = 'auto';
-    //nav.style.width = 'auto';
-    headerDesign.style.display = 'block';
-    shape1.style.display = 'block';
-  } )
+aboutBtn.addEventListener('click', () => {
+  navCo.style.display = 'none';
+  homeMenu.style.display = 'block';
+  body.style.position = 'static';
+  body.style.overflow = 'auto';
+  headerDesign.style.display = 'block';
+  shape1.style.display = 'block';
+});
 
-  contactBtn.addEventListener('click', () => {
-    navCo.style.display = 'none';
-    homeMenu.style.display = 'block';
-    //logo.style.display = 'block';
-    body.style.position = 'static';
-    body.style.overflow = 'auto';
-    //nav.style.width = 'auto';
-    headerDesign.style.display = 'block';
-    shape1.style.display = 'block';
-  } )
+contactBtn.addEventListener('click', () => {
+  navCo.style.display = 'none';
+  homeMenu.style.display = 'block';
+  body.style.position = 'static';
+  body.style.overflow = 'auto';
+  headerDesign.style.display = 'block';
+  shape1.style.display = 'block';
+});
